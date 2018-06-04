e3847389437ba71dd27c7382313e733d66f563b9
Micheal Earl <michealearldev@gmail.com>
2018-06-04 10:10:34 -0700

    Small chat client/server

--- main.js
+++ main.js
@@ -1,10 +1,24 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.get('/', function(req, res) {
-	res.send('<h1>Hello World</h1>');
+	res.sendFile(__dirname + '/index.html');
 });
 
-http.listen(8080, function() {
-	console.log("listening on port 8080");
-})
+io.on('connection', function(socket) {
+	console.log("a user connected");
+
+	socket.on('chat message', function(message) {
+		console.log(message);
+		io.emit('chat message', message);
+	});
+	
+	socket.on('disconnect', function() {
+		console.log("user disconnected");
+	});	
+});
+
+http.listen(3000, function() {
+	console.log("listening on port 3000");
+});
