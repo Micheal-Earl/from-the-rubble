71814cd7914849b346c8d6544b581796f253ebb4
Zenrix <michealearldev@gmail.com>
2018-06-06 00:55:35 -0700

    Really simple rendering of green dots. Each user controls their dot through their socket ID

--- server/main.js
+++ server/main.js
@@ -1,13 +1,26 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var path      = require('path');
 
-var path = require('path');
+var express   = require('express');
+var http      = require('http');
+var socketIO  = require('socket.io');
 
+var app       = express();
+var server    = http.Server(app);
+var io        = socketIO(server);
+
+// config
+const PORT = 3000;
+const IP = '0.0.0.0';
+
+// send index.html to web browser on GET request and use 
+// /../client for static files like style.css and script.js
+app.use(express.static(path.join(__dirname, '..', 'client')));
 app.get('/', function(req, res) {
 	res.sendFile(path.join(__dirname, '..', 'client', 'index.html'));
 });
 
+var players = {};
+// listen for the connection event
 io.on('connection', function(socket) {
 	console.log("a user connected");
 
@@ -19,8 +32,36 @@ io.on('connection', function(socket) {
 	socket.on('disconnect', function() {
 		console.log("user disconnected");
 	});	
+
+	socket.on('new player', function() {
+    players[socket.id] = {
+      x: 300,
+      y: 300
+    };
+	});
+	
+  socket.on('movement', function(data) {
+    var player = players[socket.id] || {};
+    if (data.left) {
+      player.x -= 5;
+    }
+    if (data.up) {
+      player.y -= 5;
+    }
+    if (data.right) {
+      player.x += 5;
+    }
+    if (data.down) {
+      player.y += 5;
+    }
+	});
 });
 
-http.listen(3000, function() {
-	console.log("listening on port 3000");
+setInterval(function() {
+	io.sockets.emit('state', players);
+	console.log("wtf");
+}, 1000 / 60);
+
+server.listen(PORT, function() {
+	console.log("Starting server on port " + PORT + ".");
 });
